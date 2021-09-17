e2f35761c614fbc4b3d4cd2ef41edefc494f5b81
Konrad Balon <42512720+konradBalon@users.noreply.github.com>
2021-09-17 13:25:18 +0200

    Delete venv directory

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/konrad/Desktop/ONL_PYT_W_12_podstawy_django/project/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
